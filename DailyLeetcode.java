47a0f51a42c74af9bb82f73490112cfcfc5addc1
Devang Borkar <devangborkar3@gmail.com>
2025-05-19 00:57:48 -0700

    Easiest Leetcode question

--- DailyLeetcode.java
+++ DailyLeetcode.java
@@ -1,38 +1,18 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class DailyLeetcode {
-    public String pushDominoes(String dominoes){
-        String ans = "";
-        List<List<Character>> list = new ArrayList<>();
-        for (int i = 0; i < dominoes.length(); i++) {
-            list.add(new ArrayList<>());
+    public String triangleType(int[] nums){
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+        if(set.size() == 1) return "equilateral";
+        else if(set.size() == 2) return "isosceles";
+        else{
+            if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0]) return "scalene";
+            else return "none";
         }
-        for (int i = 0; i < dominoes.length(); i++) {
-            
-            if(dominoes.charAt(i) == 'L'){
-                list.get(i).add('L');
-                int temp = i;
-                while(temp >0){
-                    if(dominoes.charAt(temp-1) == '.') list.get(i-1).add('L');
-                    temp--;
-                }
-            }
-            else if(dominoes.charAt(i) == 'R'){
-                list.get(i).add('R');
-                int temp = i;
-                while(temp < dominoes.length()-1){
-                    if(dominoes.charAt(temp+1) == '.') list.get(i-1).add('R');
-                }
-            }
-            else{
-                list.get(i).add('.');
-            }
-        }
-        for (int i = 0; i < list.size(); i++) {
-
-        }
-        return ans;
     }
     public static void main(String[] args) {
         
