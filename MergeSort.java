304034cb23742b96f758a6b939b3af1871736da0
Devang Borkar <devangborkar3@gmail.com>
2025-05-31 19:33:57 -0700

    Merge Sort

---
+++ MergeSort.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class MergeSort{
+    public int[] mergeSort(int[] arr){
+        int[] temp = new int[arr.length];
+        mergeSortRec(arr, temp, 0, arr.length-1);
+        return arr;
+    }
+    private void mergeSortRec(int[] arr, int[] temp, int start, int end) {
+        if(start >= end) return;
+        int mid = (start + end)/2;
+        mergeSortRec(arr, temp, start, mid);
+        mergeSortRec(arr, temp, mid+1, end);
+        merge(arr, temp, start, mid, end);
+    }
+    private void merge(int[] arr, int[] temp, int start, int mid, int end){
+        int left = start, right = mid+1;
+        int index = start;
+        while(left <= mid && right <= end){
+            if(arr[right] >= arr[left]){
+                temp[index] = arr[left];
+                left++;
+            }
+            else{
+                temp[index] = arr[right];
+                right++;
+            }
+            index++;
+        }
+        while(left<=mid){
+            temp[index] = arr[left];
+            left++;
+            index++;
+        }
+        while(right <= end){
+            temp[index] = arr[right];
+            right++;
+            index++;
+        }
+        for (int i = start; i <= end; i++) {
+            arr[i] = temp[i];
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,1,7,5,9,8};
+        MergeSort ms = new MergeSort();
+        System.out.println(Arrays.toString(ms.mergeSort(arr)));
+    }
+}
