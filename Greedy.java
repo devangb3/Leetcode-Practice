7a1ef9d8ec9aefda446eb7990486c568d6b7ae61
Devang Borkar <devangborkar3@gmail.com>
2025-07-30 20:23:48 -0700

    Refactor canJump method and update main to test jump method

--- Greedy.java
+++ Greedy.java
@@ -10,9 +10,6 @@ public class Greedy {
         return sum;
     }
     public boolean canJump(int[] nums) {
-        if(nums.length == 1) return true;
-        if(nums[0] == 0) return false;
-
         int maxReach = nums[0];
         for (int i = 0; i < nums.length; i++) {
             maxReach = Math.max(maxReach, i + nums[i]);
@@ -21,8 +18,26 @@ public class Greedy {
         }
         return false;
     }
+
+    public int jump(int[] nums) {
+       int numberOfSteps = 0;
+       int currentReach = 0;
+       int bestJump = 0;
+       for (int i = 0; i < nums.length-1; i++) {
+            bestJump = Math.max(bestJump, i + nums[i]);
+            if(i == currentReach){
+                numberOfSteps++;
+                currentReach = bestJump;
+            }
+       }
+       return numberOfSteps;
+    }
+
+    
     public static void main(String[] args) {
         Greedy g = new Greedy();
-        System.out.println(g.canJump(new int[]{1,2,3}));
+        
+        System.out.println(g.jump(new int[]{2,3,1,1,4}));
+        
     }
 }
