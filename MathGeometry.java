d3e598fc343f12f838767960b3b1b083189e96b9
Devang Borkar <devangborkar3@gmail.com>
2025-08-16 14:12:35 -0700

    Spiral Matrix

--- MathGeometry.java
+++ MathGeometry.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MathGeometry {
     public void rotate(int[][] matrix){
@@ -20,8 +22,37 @@ public class MathGeometry {
 
         for(int[] row : matrix) System.out.println(Arrays.toString(row));
     }
+    
+    public List<Integer> spiralOrder(int[][] matrix){
+        List<Integer> ans = new ArrayList<>();
+        int left = 0, right = matrix[0].length;
+        int top = 0, bottom = matrix.length;
+        while(left < right && top < bottom){
+            for (int i = left; i < right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i < bottom; i++) {
+                ans.add(matrix[i][right-1]);
+            }
+            right--;
+            if (left >= right || top >= bottom) {
+                break;
+            }
+            
+            for (int i = right-1; i >= left; i--) {
+                ans.add(matrix[bottom-1][i]);
+            }
+            bottom--;
+            for (int i = bottom-1; i >= top; i--) {
+                ans.add(matrix[i][left]);
+            }
+            left++;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         MathGeometry mg = new MathGeometry();
-        mg.rotate(new int[][]{{5,1,9,11}, {2,4,8,10}, {13,3,6,7}, {15,14,12,16}});
+        System.out.println(mg.spiralOrder(new int[][]{{1,2,3}}));
     }
 }
