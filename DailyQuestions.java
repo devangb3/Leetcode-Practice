4e67d5b6d2857fc16ae466337073215e599be375
Devang Borkar <devangborkar3@gmail.com>
2025-10-10 15:22:18 -0700

    3147. Taking Maximum Energy From the Mystic Dungeon

--- DailyQuestions.java
+++ DailyQuestions.java
@@ -435,10 +435,17 @@ public class DailyQuestions {
         }
         return closestIndex;
     }
+    public int maximumEnergy(int[] energy, int k) {
+        int maximumEnergy = Integer.MIN_VALUE;
+        for (int i = energy.length-1; i >= 0; i--) {
+            if(i+k >= energy.length) continue;
+            energy[i] += energy[i+k];
+        }
+        for(int en : energy) maximumEnergy = Math.max(en, maximumEnergy);
+        return maximumEnergy;
+    }
     public static void main(String[] args) {
         DailyQuestions d = new DailyQuestions();
-        int[] spells = new int[]{40,11,24,28,40,22,26,38,28,10,31,16,10,37,13,21,9,22,21,18,34,2,40,40,6,16,9,14,14,15,37,15,32,4,27,20,24,12,26,39,32,39,20,19,22,33,2,22,9,18,12,5};
-        int[] potions = new int[]{31,40,29,19,27,16,25,8,33,25,36,21,7,27,40,24,18,26,32,25,22,21,38,22,37,34,15,36,21,22,37,14,31,20,36,27,28,32,21,26,33,37,27,39,19,36,20,23,25,39,40};
-        System.out.println(Arrays.toString(d.successfulPairs(spells, potions, 600)));
+        System.out.println(d.maximumEnergy(new int[]{-2,-3,-1}, 2));
     }
 }
